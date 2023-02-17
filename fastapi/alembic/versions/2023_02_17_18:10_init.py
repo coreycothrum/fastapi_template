2efacc880051801c4de9a0bfc4214b51233bea27
Corey Cothrum <contact@coreycothrum.com>
2023-02-17 19:24:04 +0000

    chore(alembic): init (empty) migration
    generated with: `alembic revision -m "init"`

---
+++ fastapi/alembic/versions/2023_02_17_18:10_init.py
@@ -0,0 +1,24 @@
+"""init
+
+Revision ID: ad6064743414
+Revises:
+Create Date: 2023-02-17 18:10:09.506753+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "ad6064743414"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
